f50112717080e06219f2e3fb8d0173129e209f81
Jackson Mades <jacksonmades@gmail.com>
2018-06-19 21:45:47 -0400

    I have gotten it down to 11 failures out of 21. Will has it on 6 failures. I am at a roadblock, this rome will not be built for a long while.

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,40 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+    this.cards = cards;
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+};
 
-// };
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+    var m = cardsArr.length, t, i;
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    // While there remain elements to shuffle…
+    while (m) {
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * m--);
 
-// }
+        // And swap it with the current element.
+        t = cardsArr[m];
+        cardsArr[m] = cardsArr[i];
+        cardsArr[i] = t;
+    }
+    return cardsArr;
+};
 
-// MemoryGame.prototype.finished = function () {
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
 
-// };
+};
+
+MemoryGame.prototype.finished = function () {
+    if((this.cards.length)/2 == this.pickedCards){
+        return "Win"
+    }
+};
+
+MemoryGame.prototype.pickedCards = function () {
+
+};
+
+MemoryGame.prototype.pairsClicked = function () {
+
+};
+
+
